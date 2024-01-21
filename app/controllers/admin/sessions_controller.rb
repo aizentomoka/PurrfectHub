b2b0c396cac790291db56ca3b88a3092c247057e
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2024-01-21 06:46:17 +0000

    [update]layout/admin/session/new

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -10,6 +10,10 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     root_path
   end
+  
+  def after_sign_out_path_for(resource)
+    admin_session_path
+  end
   # POST /resource/sign_in
   # def create
   #   super
