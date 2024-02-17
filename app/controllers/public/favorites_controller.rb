75e297976d199c85e00189db5aa3607d4cde3a3a
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2024-02-17 12:41:47 +0000

    [update]favorite_Ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,18 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
+  
   def create
-    diary = Diary.find(params[:diary_id])
-    favorite = current_user.favorites.new(diary_id: diary.id)
+    @diary = Diary.find(params[:diary_id])
+    favorite = current_user.favorites.new(diary_id: @diary.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    diary = Diary.find(params[:diary_id])
-    favorite = current_user.favorites.find_by(diary_id: diary.id)
+    @diary = Diary.find(params[:diary_id])
+    favorite = current_user.favorites.find_by(diary_id: @diary.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
 end
